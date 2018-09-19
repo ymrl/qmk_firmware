2df9f957e615caaad1299e4a7eb512885f947460
ymrl <ymrl@ymrl.net>
2018-09-19 14:47:06 +0900

    Add BKSP

--- keyboards/ergo42/keymaps/ymrl/keymap.c
+++ keyboards/ergo42/keymaps/ymrl/keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     KC_RBRC,       KC_LBRC, KC_Y,     KC_U,    KC_I,     KC_O,   KC_P,    KC_BSPC, \
-    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     KC_LPRN,       KC_RPRN, KC_H,     KC_J,    KC_K,     KC_L,   KC_SCLN, KC_ENT, \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_LCBR,       KC_RCBR, KC_N,     KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS, \
-    KC_LCTRL, KC_ESC,  KC_DELT, KC_LALT, KC_LGUI,  MO(SYMB), SFT_T(KC_SPC), KC_RSFT, MO(META), KC_RGUI, KC_LEFT,  KC_UP,  KC_DOWN, KC_RIGHT \
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,          KC_RBRC,  KC_LBRC,  KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,    KC_BSPC, \
+    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,          KC_LPRN,  KC_RPRN,  KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN, KC_ENT, \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,          KC_LCBR,  KC_RCBR,  KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS, \
+    KC_LCTRL, KC_ESC,  KC_DELT, KC_LALT, KC_LGUI,  SFT_T(KC_SPC), MO(SYMB), MO(META), KC_RSFT, KC_BSPC, KC_LEFT,  KC_UP,  KC_DOWN, KC_RIGHT \
   ),
 
   /* META
