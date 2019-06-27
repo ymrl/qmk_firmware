c96916b1998e85e198a3bbdc99ead9eec76411ed
ymrl <ymrl@ymrl.net>
2019-06-27 13:58:43 +0900

    Change Ergodash mini as MASTER_RIGHT

--- keyboards/ergodash/mini/keymaps/ymrl/keymap.c
+++ keyboards/ergodash/mini/keymaps/ymrl/keymap.c
@@ -35,15 +35,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_QWERTY] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_MINS,                   KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_LCBR,                   KC_RCBR, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC,                   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_LBRC,                   KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LCBR,                   KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
         KC_ESC,  KC_LGUI, KC_LALT,          KC_LGUI, KC_SPC, LOWER,   KC_BSPC, KC_RSFT, RAISE,   KC_RALT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
     /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
    * | Esc  |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  `   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |      |   -  |   =  |   [  |   ]  |  '   |
+   * | Ctrl |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |      |   _  |   +  |   [  |   ]   |   "  |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   6  |   7  |   8  |   9  |   0  |   {  |                    |   }  |   N  |   M  |   <  |   >  |   ?  |  \   |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_LOWER] = LAYOUT(
         KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                   KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_GRV,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                   KC_RBRC, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                   KC_RBRC, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO,
         _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LCBR,                   KC_RCBR, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,          KC_HOME, KC_PGDN, KC_PGUP, KC_END),
 
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,----------------------------------------------------------------------------------------------------------------------.
   * | Esc  |   1  |   2  |   3  |   4  |   5  |   _  |                    |   +  |   6  |   7  |   8  |  9   |   0  |  ~   |
   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-  * | Ctrl |   F1 |   F2 |   F3 |   F4 |   F5 |   {  |                    |   }  |  F6  |  _  |   +  |   [  |   ]   |   "  |
+  * | Ctrl |   F1 |   F2 |   F3 |   F4 |   F5 |   {  |                    |   }  |  F6  |   -  |   =  |   [  |   ]  |  '   |
   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
   * | Shift|   F6 |   F7 |   F8 |   F9 |  F10 | Space|                    | Enter|  F12 |   M  |   <  |   >  |   ?  |  |   |
   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
     [_RAISE] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_UNDS,                   KC_PLUS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LBRC,                   KC_RBRC, KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LBRC,                   KC_RBRC, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
         _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_LCBR,                   KC_RCBR, KC_F12,  _______, _______, _______, _______, KC_PIPE,
         _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,          KC_HOME, KC_PGDN, KC_PGUP, KC_END),
 
